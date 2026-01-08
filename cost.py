a8e15c6d8bb318d0e00ac4ba0b5d0b405cf7c89b
Simon Svane <simonsvane4@gmail.com>
2026-01-08 13:00:29 +0100

    improved runtime

--- cost.py
+++ cost.py
@@ -1,24 +1,25 @@
-from feasibilitycheck import load_vrp_data
+# cost.py
+# (No imports needed here if we just do math, but keeping structure)
 
 def calculate_route_cost(solution, customer_addr_idx, time_matrix_array, depot_idx):
-   
+    """
+    Calculates the total travel time for a route.
+    Optimized for raw list/array access.
+    """
+    if not solution:
+        return 0.0
+
     total_travel_time = 0.0
     last_idx = depot_idx
 
     # Traverse route from depot through all customers
+    # solution is expected to be a list of customer INDICES (ints)
     for cust_row_idx in solution:
         current_idx = customer_addr_idx[cust_row_idx]
         total_travel_time += time_matrix_array[last_idx, current_idx]
         last_idx = current_idx
 
-    # Return to depot if route is non-empty
-    if solution:
-        total_travel_time += time_matrix_array[last_idx, depot_idx]
+    # Return to depot
+    total_travel_time += time_matrix_array[last_idx, depot_idx]
 
     return total_travel_time
-
-
-customers, vehicles, time_matrix_df, time_matrix_array, address_to_idx, customer_addr_idx, customer_arrays = load_vrp_data()
-depot_idx = address_to_idx['Depot']
-calculate_route_cost([1,2,3], customer_addr_idx, time_matrix_array, depot_idx)
-
