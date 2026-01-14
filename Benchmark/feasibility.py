5ebdc26cb6a88821b9c7eba47f4e9ebb29821391
Simon Svane <simonsvane4@gmail.com>
2026-01-14 12:47:57 +0100

    Implemented q-learning engine + more operators

--- Benchmark/feasibility.py
+++ Benchmark/feasibility.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-def load_vrp_data(customers_file='R106/customers.csv', vehicles_file='R106/vehicles.csv', distance_matrix_file='R106/distance_matrix.csv'):
+def load_vrp_data(customers_file='C101/customers.csv', vehicles_file='C101/vehicles.csv', distance_matrix_file='C101/distance_matrix.csv'):
     # Get the directory of this script
     script_dir = os.path.dirname(os.path.abspath(__file__))
     
